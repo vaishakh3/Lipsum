9fbbd47f90759522ba9c1e2f067ba082601cd6b5
vaishakh3 <72205266+vaishakh3@users.noreply.github.com>
2023-05-17 22:25:34 +0530

    Update App.jsx

--- Lipsum-frontend/src/App.jsx
+++ Lipsum-frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Landing from './pages/Landing/Landing'
+import Home from "./pages/Home/Home";
+import Initial from "./pages/Initial/Initial";
+import Quiz from './pages/Quiz/Quiz';
 
 function App() {
 
   return (
     <div className="App">
       <Landing/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Initial />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Quiz" element={<Quiz/>} />
+        </Routes>
+
+       </BrowserRouter>
     </div>
   )
 }
